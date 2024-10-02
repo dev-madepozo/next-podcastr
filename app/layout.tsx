6c8a83b84aa9d522a7a86463bc49cc76bd63296a
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-10-01 23:40:36 -0500

    Add convex setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+import { ConvexClientProvider } from "./providers/ConvexProvider";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -31,7 +33,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ConvexClientProvider>
+          {children}
+        </ConvexClientProvider>
       </body>
     </html>
   );
--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -8,11 +8,15 @@ interface PodcastCardProps {
   imgUrl: string
 }
 
-const PodcastCard = ({ description, title, imgUrl, podcastId }: PodcastCardProps) => {
+const PodcastCard = ({ description, title, imgUrl }: PodcastCardProps) => {
   return (
     <div className='cursor-pointer'>
       <figure className='flex flex-col gap-2'>
-        <Image src={imgUrl} width={174} height={174} alt={title} />
+        <Image src={imgUrl} width={174} height={174} alt={title} className='aspect-square h-fit w-full rounded-xl 2xl:size-[200px' />
+        <div className="flex flex-col">
+          <h1 className='text-16 truncate font-bold text-white-1'>{title}</h1>
+          <h2 className='text-12 truncate font-normal capitalize text-white-1'>{description}</h2>
+        </div>
       </figure>
     </div>
   )
