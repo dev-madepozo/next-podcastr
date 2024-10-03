cf585e1230e2c99290e410b3da2e4fadb309d0d9
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-10-02 23:46:36 -0500

    Setup clerk with convex

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,19 +2,13 @@
 
 import PodcastCard from '@/components/PodcastCard'
 import { podcastData } from '@/constants'
-import { useQuery } from "convex/react"
-import { api } from "@/convex/_generated/api"
 
 const Home = () => {
-  const tasks = useQuery(api.tasks.get)
 
   return (
     <div className='mt-9 flex flex-col gap-9'>
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
-        <div className='text-white-1'>
-          {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
-        </div>
         <div className="podcast_grid">
           {podcastData.map(({ id, imgURL, title, description }) => (
             <PodcastCard
---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+// https://balanced-goat-61.clerk.accounts.dev
+
+const authConfig = {
+  providers: [
+    {
+      domain: 'https://balanced-goat-61.clerk.accounts.dev',
+      applicationID: 'convex'
+    }
+  ]
+}
+
+export default authConfig
