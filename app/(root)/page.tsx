cbee834383f5db77643b2fdc1f68f825d06bd70d
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-10-07 23:00:48 -0500

    Updates

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,6 @@ import PodcastCard from '@/components/PodcastCard'
 import { podcastData } from '@/constants'
 
 const Home = () => {
-
   return (
     <div className='mt-9 flex flex-col gap-9'>
       <section className="flex flex-col gap-5">
--- app/providers/ConvexClerkProvider.tsx
+++ app/providers/ConvexClerkProvider.tsx
@@ -1,7 +1,8 @@
+import { ReactNode } from 'react';
+
 import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ConvexReactClient } from "convex/react";
-import { ReactNode } from 'react';
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
 
